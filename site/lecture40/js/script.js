6578d5989437d7a50a18aeecfe9d6d476e99d91a
sawantaniket82 <sawantaniket82@gmail.com>
2021-04-29 14:23:40 +0530

    29-apr2021

---
+++ site/lecture40/js/script.js
@@ -0,0 +1,22 @@
+/*var a="my name"
+console.log("global :msaage =" + a);
+
+var b =function(){
+    var a = "is Aniket"
+    console.log("global : message =" + a);
+    function c(){
+	console.log("Ankush Sawant" + a);
+}
+    c();
+
+}
+
+
+b();*/
+
+
+var x =function(){
+	console.log("heelo world");
+	return;
+
+};x();
